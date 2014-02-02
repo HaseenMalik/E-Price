65c46a0cce769df1e28a2bdc95fe031e7706ab6b
HaseenMalik <haseen160@gmail.com>
2014-02-02 10:10:23 +0500

    readme updated

---
+++ README.md
@@ -0,0 +1,42 @@
+E-Price List 1.0 README FILE
+===============================
+E-Price List DESCRIPTION
+-----------------------
+Problem Statment:
+.................
+	As we know that now a days shopkeepers have 
+their own desired Prices, the government needs to control this.
+This problem can be solved if everyone have access 
+to government standardized price list.
+
+
+
+Solution:
+........
+	We are developing an app to keep the customers updated 
+according to new prices.
+Also this app will have functionalty of reporting authorities 
+about any non-regularities.
+
+How to use?
+...........
+
+1) Go to "create message" option of your mobile.
+
+2) Type "PCHC item_name", e.g "PCHC Milk"
+
+3) Send to 8333.
+
+4) You will recive the price of item.
+
+FEATURES:
+...........
+
+	The important feature of this app is the quick responce to the qurry with
+just a single SMS. 
+
+
+
+
+
+
--- sendsms.php
+++ sendsms.php
@@ -1,4 +1,5 @@
 <?php session_start();
+include_once("api_class.php");
 set_time_limit(90);
 error_reporting(E_ALL ^ E_NOTICE);
 //include_once("api_class.php");
@@ -7,10 +8,9 @@ $con = mysql_connect("localhost", "root", "");
 mysql_select_db("e-price", $con);
 
 
-
-
 //$status = $object_smile_api->get_session();
-if(isset($_SESSION["sid"])){
+/*
+if(!isset($_SESSION["sid"])){
 	$sess = file_get_contents("http://api.smilesn.com/session?username=4&password=Moonbeam8333");
 	$sess = json_decode($sess);
 	$sess = $sess->sessionid;
@@ -19,9 +19,11 @@ if(isset($_SESSION["sid"])){
 	//print_r($sess);
 	#exit;
 }
+*/
 
 
-$receive = json_decode(file_get_contents("http://api.smilesn.com/receivesms?sid=" . $_SESSION["sid"]));
+
+$receive = json_decode($object_smile_api->receive_sms());
 
 foreach($receive as $value){
 		
@@ -29,22 +31,23 @@ foreach($receive as $value){
 	var_dump($single);
 	echo $sender =  $single->sender_num;
 	echo $text   = $single->text;
+	if(!empty($sender))
+	{
 	$qry = mysql_query("SELECT price FROM priceList WHERE variation LIKE '%".$text."%'");
 	$res = mysql_fetch_array($qry);
 	echo $price = $res[0];	
-	$status = json_decode(file_get_contents("http://api.smilesn.com/sendsms?sid="
-											 . $_SESSION["sid"]
-											 . "&sendernum=8333"
-											 . "&receivenum=". str_replace("+92", "0", $sender)
-											 . "&textmessage=" . urlencode($text . " price is Rs." . $price)));
+	$sender = str_replace("+92", "0", $sender);
+	$txtmsg = $text . " price is Rs." . $price;
+	$status = json_decode($object_smile_api->send_sms($sender,8333,$txtmsg));
 	if($status){
 		var_dump($status);
+	}
 	}
 }
 
 
 	$url=$_SERVER['REQUEST_URI'];
-	header("Refresh: 5; URL=\"" . $url . "\""); // redirect in 5 seconds
+	header("Refresh: 1; URL=\"" . $url . "\""); // redirect in 1 seconds
 
 
 
